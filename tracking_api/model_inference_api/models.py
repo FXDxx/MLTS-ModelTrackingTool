2a745955b0418844b49468c10b0bfe3fcb9fbb03
Faizan <mfaizan33198@gmai.com>
2026-01-27 10:26:25 +0500

    Inference and tracking

--- tracking_api/model_inference_api/models.py
+++ tracking_api/model_inference_api/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from model_tracking_api.models import MLModels as MLModel
-from dataset_tracking_api.models import Dataset
+from dataset_tracking_api.models import Datasets
 # Create your models here.
 
 class ModelInference(models.Model):
     model = models.ForeignKey(MLModel, on_delete=models.CASCADE) 
-    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE)
+    dataset = models.ForeignKey(Datasets, on_delete=models.CASCADE)
     parameter_used = models.JSONField(default=dict, blank=True)
     metrics = models.JSONField(default=dict, blank=True)
     status = models.CharField(max_length=50, default='pending')
--- tracking_api/model_inference_api/views.py
+++ tracking_api/model_inference_api/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.decorators import action
-from rest_framework.response import Response
+from .models import ModelInference
 from .serializers import ModelInferenceSerializer
 
 # Create your views here.
 class ModelInferenceViewset(ModelViewSet):
-    queryset = ModelInferenceSerializer.objects.all()
+    queryset = ModelInference.objects.all()
     serializer_class = ModelInferenceSerializer
